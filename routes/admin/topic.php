3eac0529ad3319951b5cba6319e0160bf0f3df40
eleven <yinzhengfan@163.com>
2026-01-30 11:22:42 +0800

    修改(Route): Admin 模块话题功能路由

--- routes/admin/topic.php
+++ routes/admin/topic.php
@@ -16,34 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('system.auth')->group(function () {
     Route::prefix('admin/topic')->group(function () {
         // 话题列表
-        Route::get('/list', [TopicController::class, 'list'])
-            ->middleware('permission:admin:topic:list');
-        
+        Route::get('/list', [TopicController::class, 'list']);
+
         // 下拉选项（无需权限控制，供其他模块使用）
         Route::get('/optionselect', [TopicController::class, 'optionselect']);
-        
+
         // 话题详情
-        Route::get('/{topicId}', [TopicController::class, 'show'])
-            ->middleware('permission:admin:topic:query');
-        
+        Route::get('/{topicId}', [TopicController::class, 'show']);
+
         // 新增话题
-        Route::post('/', [TopicController::class, 'store'])
-            ->middleware('permission:admin:topic:add');
-        
+        Route::post('/', [TopicController::class, 'store']);
+
         // 更新话题
-        Route::put('/', [TopicController::class, 'update'])
-            ->middleware('permission:admin:topic:edit');
-        
+        Route::put('/', [TopicController::class, 'update']);
+
         // 删除话题（支持批量）
-        Route::delete('/{topicIds}', [TopicController::class, 'destroy'])
-            ->middleware('permission:admin:topic:remove');
-        
+        Route::delete('/{topicIds}', [TopicController::class, 'destroy']);
+
         // 修改状态
-        Route::put('/changeStatus', [TopicController::class, 'changeStatus'])
-            ->middleware('permission:admin:topic:edit');
-        
+        Route::put('/changeStatus', [TopicController::class, 'changeStatus']);
+
         // 设置推荐
-        Route::put('/changeRecommend', [TopicController::class, 'changeRecommend'])
-            ->middleware('permission:admin:topic:edit');
+        Route::put('/changeRecommend', [TopicController::class, 'changeRecommend']);
     });
 });

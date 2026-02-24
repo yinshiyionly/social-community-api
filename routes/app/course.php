ec3d6debb83c72040a048953f0b2c051d23a2d8e
eleven <yinzhengfan@163.com>
2026-02-24 11:39:01 +0800

    修改(App): App 模块-课程详情接口使用可选登录中间件

--- routes/app/course.php
+++ routes/app/course.php
@@ -18,7 +18,8 @@ Route::prefix('v1/course')->group(function () {
     Route::get('/list', [CourseController::class, 'listByCategory']);
 
     // 课程详情（可选登录，登录后返回是否已拥有）
-    Route::get('/detail', [CourseController::class, 'detail'])->middleware('app.jwt.optional');
+    // Route::get('/detail', [CourseController::class, 'detail'])->middleware('app.jwt.optional');
+    Route::get('/detail', [CourseController::class, 'detail'])->middleware('app.auth.optional');
 
     // 好课上新列表
     Route::get('/new', [CourseController::class, 'newCourses']);

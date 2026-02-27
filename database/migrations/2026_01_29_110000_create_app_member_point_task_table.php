3b48689d4de591b4b2e057ecd91917c14772f2ec
eleven <yinzhengfan@163.com>
2026-02-27 14:00:22 +0800

    修改(Migrate): 调整用户积分任务配置表名称

--- database/migrations/2026_01_29_110000_create_app_member_point_task_table.php
+++ database/migrations/2026_01_29_110000_create_app_member_point_task_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 
-class CreateAppPointTaskTable extends Migration
+class CreateAppMemberPointTaskTable extends Migration
 {
     /**
      * Run the migrations.

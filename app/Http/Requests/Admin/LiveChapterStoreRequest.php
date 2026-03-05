d444ebb7d5b66f9f9670b8a760a351ea21fc87c7
eleven <yinzhengfan@163.com>
2026-03-05 10:27:07 +0800

    修改(Admin): Admin模块-直播章节验证类参数调整为小驼峰格式

--- app/Http/Requests/Admin/LiveChapterStoreRequest.php
+++ app/Http/Requests/Admin/LiveChapterStoreRequest.php
@@ -14,27 +14,27 @@ class LiveChapterStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'chapter_title' => 'required|string|max:100',
-            'live_start_time' => 'required|date',
-            'live_end_time' => 'required|date|after:live_start_time',
-            'is_free' => 'required|integer|in:0,1',
-            'room_id' => 'required|integer|exists:app_live_room,room_id',
+            'chapterTitle' => 'required|string|max:100',
+            'liveStartTime' => 'required|date',
+            'liveEndTime' => 'required|date|after:live_start_time',
+            'isFree' => 'required|integer|in:0,1',
+            'roomId' => 'required|integer|exists:app_live_room,room_id',
         ];
     }
 
     public function messages()
     {
         return [
-            'chapter_title.required' => '章节标题不能为空。',
-            'chapter_title.max' => '章节标题不能超过100个字符。',
-            'live_start_time.required' => '直播开始时间不能为空。',
-            'live_start_time.date' => '直播开始时间格式不正确。',
-            'live_end_time.required' => '直播结束时间不能为空。',
-            'live_end_time.after' => '直播结束时间必须晚于开始时间。',
-            'is_free.required' => '请选择是否免费试看。',
-            'is_free.in' => '免费试看值不正确。',
-            'room_id.required' => '请选择直播间。',
-            'room_id.exists' => '直播间不存在。',
+            'chapterTitle.required' => '章节标题不能为空。',
+            'chapterTitle.max' => '章节标题不能超过100个字符。',
+            'liveStartTime.required' => '直播开始时间不能为空。',
+            'liveStartTime.date' => '直播开始时间格式不正确。',
+            'liveEndTime.required' => '直播结束时间不能为空。',
+            'liveEndTime.after' => '直播结束时间必须晚于开始时间。',
+            'isFree.required' => '请选择是否免费试看。',
+            'isFree.in' => '免费试看值不正确。',
+            'roomId.required' => '请选择直播间。',
+            'roomId.exists' => '直播间不存在。',
         ];
     }
 }

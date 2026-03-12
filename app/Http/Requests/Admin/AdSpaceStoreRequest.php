b2a9322484790daeb480495c76f053f7115088ef
eleven <yinzhengfan@163.com>
2026-03-12 10:58:34 +0800

    修改(验证类): Admin模块-广告位增加验证规则
    1. 验证广告位名称唯一性
    2. 最大广告数必须大于0

--- app/Http/Requests/Admin/AdSpaceStoreRequest.php
+++ app/Http/Requests/Admin/AdSpaceStoreRequest.php
@@ -15,18 +15,23 @@ class AdSpaceStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            'spaceName' => 'required|string|max:50',
+            'spaceName' => [
+                'required',
+                'string',
+                'max:50',
+                Rule::unique('app_ad_space', 'space_name')->whereNull('deleted_at')
+            ],
             'spaceCode' => [
                 'required',
                 'string',
                 'max:50',
                 Rule::unique('app_ad_space', 'space_code')->whereNull('deleted_at'),
             ],
-            'platform' => 'nullable|integer|in:0,1,2',
-            'width' => 'nullable|integer|min:0',
-            'height' => 'nullable|integer|min:0',
-            'maxAds' => 'nullable|integer|min:0',
-            'status' => 'nullable|integer|in:1,2',
+            'platform'  => 'nullable|integer|in:0,1,2',
+            'width'     => 'nullable|integer|min:0',
+            'height'    => 'nullable|integer|min:0',
+            'maxAds'    => 'nullable|integer|min:1',
+            'status'    => 'nullable|integer|in:1,2',
         ];
     }
 
@@ -34,15 +39,16 @@ class AdSpaceStoreRequest extends FormRequest
     {
         return [
             'spaceName.required' => '广告位名称不能为空',
-            'spaceName.max' => '广告位名称不能超过50个字符',
+            'spaceName.max'      => '广告位名称不能超过50个字符',
+            'spaceName.unique'   => '广告位名称已存在',
             'spaceCode.required' => '广告位编码不能为空',
-            'spaceCode.max' => '广告位编码不能超过50个字符',
-            'spaceCode.unique' => '广告位编码已存在',
-            'platform.in' => '平台值无效',
-            'width.min' => '宽度不能小于0',
-            'height.min' => '高度不能小于0',
-            'maxAds.min' => '最大广告数不能小于0',
-            'status.in' => '状态值无效',
+            'spaceCode.max'      => '广告位编码不能超过50个字符',
+            'spaceCode.unique'   => '广告位编码已存在',
+            'platform.in'        => '平台值无效',
+            'width.min'          => '宽度不能小于0',
+            'height.min'         => '高度不能小于0',
+            'maxAds.min'         => '最大广告数必须大于0',
+            'status.in'          => '状态值无效',
         ];
     }
 }

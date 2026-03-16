ea147da0e7ca323d1b13649be5b8d5e4ba7543c0
eleven <yinzhengfan@163.com>
2026-03-16 15:17:17 +0800

    新增(Config): 增加 suxuedao 数据库配置

--- config/database.php
+++ config/database.php
@@ -63,6 +63,25 @@ return [
             ]) : [],
         ],
 
+        'suxuedao' => [
+            'driver' => env('DB_SUXUEDAO_CONNECTION', 'mysql'),
+            'host' => env('DB_SUXUEDAO_HOST', '127.0.0.1'),
+            'port' => env('DB_SUXUEDAO_PORT', '3306'),
+            'database' => env('DB_SUXUEDAO_DATABASE', 'forge'),
+            'username' => env('DB_SUXUEDAO_USERNAME', 'forge'),
+            'password' => env('DB_SUXUEDAO_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),

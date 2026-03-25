4822bb82f2b6320ef7aa085cc671f6745a17423a
eleven <yinzhengfan@163.com>
2026-03-25 09:52:04 +0800

    修改(资源类): Admin模块-课程订单退款详情

---
+++ app/Http/Resources/Admin/CourseRefundDetailResource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Resources\Admin;
+
+/**
+ * 课程退款详情资源。
+ *
+ * 当前结构与订单详情一致，单独保留资源类用于后续退款专属字段扩展。
+ */
+class CourseRefundDetailResource extends CourseOrderDetailResource
+{
+}

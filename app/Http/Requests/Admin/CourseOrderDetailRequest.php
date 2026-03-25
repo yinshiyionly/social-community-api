47cebf646052e2ea5612d9399a8718659a81b59d
eleven <yinzhengfan@163.com>
2026-03-25 09:51:07 +0800

    修改(验证类): Admin模块-课程订单详情

---
+++ app/Http/Requests/Admin/CourseOrderDetailRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * 课程订单详情请求验证。
+ */
+class CourseOrderDetailRequest extends FormRequest
+{
+    /**
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function rules()
+    {
+        return [
+            'orderNo' => 'required|string|max:64',
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages()
+    {
+        return [
+            'orderNo.required' => '订单号不能为空',
+            'orderNo.max' => '订单号长度不能超过64位',
+        ];
+    }
+}
